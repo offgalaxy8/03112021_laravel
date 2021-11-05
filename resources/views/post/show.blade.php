49965014ddbf0e0a139bdd3b98c904f9cf1a3b9f
Sergey <s.makaronok@ostrovchistoty.by>
2021-11-05 10:05:11 +0300

    lesson 17 end

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.main')
 @section('content')
-  <div>
-      <h2>{{ $post->id }}. {{ $post->title }}</h2>
-      <p>{{ $post->id }}. {{ $post->content }}</p>
-      <p>{{ $post->id }}. {{ $post->image }}</p>
-      <p>{{ $post->id }}. {{ $post->likes }}</p>
-  </div>
+    <div>
+        <h2>{{ $post->id }}. {{ $post->title }}</h2>
+        <p>{{ $post->id }}. {{ $post->content }}</p>
+        <p>{{ $post->id }}. {{ $post->image }}</p>
+        <p>{{ $post->id }}. {{ $post->likes }}</p>
+    </div>
+
+    <div class="bottom-navi">
+        <a href="{{ route('post.edit', $post->id) }}">Edit</a>
+    </div>
+
+    <div class="bottom-navi">
+        <form action="{{ route('post.delete', $post->id) }}" method="post">
+            @csrf
+            @method('delete')
+            <button class="btn btn-danger mb-3 mt-3" type="submit">Delete</button>
+        </form>
+    </div>
+
+    <div class="bottom-navi">
+        <a href="{{ route('post.index') }}">Back</a>
+    </div>
 @endsection
